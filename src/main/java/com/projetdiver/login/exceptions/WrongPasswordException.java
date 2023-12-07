a1e8e3f3bd7258fa0974d8c101209070afab5720
NathanaelCostes <nathanaelcostes7@gmail.com>
2023-12-07 17:30:59 +0100

    feat(login) : Ajout gestion erreur

---
+++ src/main/java/com/projetdiver/login/exceptions/WrongPasswordException.java
@@ -0,0 +1,14 @@
+package com.projetdiver.login.exceptions;
+
+/**
+ * Exception raised when the password is wrong
+ */
+public class WrongPasswordException extends Exception {
+    /**
+     * Creates a WrongPasswordException
+     * @param errorMessage
+     */
+    public WrongPasswordException(String errorMessage) {
+        super(errorMessage);
+    }
+}
