68b785e45bb60182d14f797741e80ffb87ac3ed1
Léo <damerval.leo@proton.me>
2023-12-30 18:55:27 +0100

    feat: Test Impl FXRouter

---
+++ src/main/java/com/projetdiver/MainApplication.java
@@ -0,0 +1,42 @@
+package com.projetdiver;
+
+import com.projetdiver.diver.Diver;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Main class of the application
+ */
+public class MainApplication extends Application {
+    /**
+     * Main method of the application. Launches the javafx application.
+     * @param args arguments of the application
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+
+    /**
+     * Starts the javafx application
+     * @param stage the stage of the application
+     * @throws IOException if the fxml file is not found
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXRouter.bind(this, stage);
+        FXMLLoader fxmlLoader = new FXMLLoader(DiverApplication.class.getResource("/com/projetdiver/views/account/login-view.fxml"));
+        System.out.println(fxmlLoader.getLocation());
+        //Convert fxmlLoader URL to String :
+        String fxmlLoaderURL = fxmlLoader.getLocation().toString();
+        System.out.println(fxmlLoaderURL);
+        /*FXRouter.when("login", DiverApplication.class.getResource("/com/projetdiver/views/account/login-view.fxml").toString());
+        FXRouter.goTo("login");*/
+    }
+}
