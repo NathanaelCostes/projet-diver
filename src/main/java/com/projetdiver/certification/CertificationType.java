b487210a247ec95bdee8d7343e99b73c118cd44a
NathanaelCostes <nathanaelcostes7@gmail.com>
2024-01-05 16:33:24 +0100

    feat(certification) : added what I forgot to add

---
+++ src/main/java/com/projetdiver/certification/CertificationType.java
@@ -0,0 +1,18 @@
+package com.projetdiver.certification;
+
+public enum CertificationType {
+    AutonomousDiver,
+    SupervisedDiver,
+    DiveGuide,
+    NitroxDiver,
+    TrimixDiver,
+    CaveDiver,
+    DeepDiver,
+    NavigationDiver,
+    PhotographyDiver,
+    BiologyDiver,
+    ArchaeologyDiver,
+    EnvironmentalDiver,
+    RescueDiver,
+    HandicappedDiver,
+}
